aaeac989b0bb4010a32e7b8c97c108bb532b9c58
mijelpalcabello <miguel.castillo861999@gmail.com>
2023-03-25 14:00:53 -0500

    feat: agregando seeders

---
+++ database/seeders/CursoSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Curso;
+use Illuminate\Database\Seeder;
+
+class CursoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $curso = new Curso();
+        $curso->name = "Javascript";
+        $curso->description = "El peor lenguaje";
+
+        $curso->save();
+    }
+}
